e378f17a9189b3dda3bd425bdb3a71adedc6a243
jiang <790495598@qq.com>
2023-04-16 21:58:41 +0800

    测试push

--- src/main/java/thread/FunDemo05.java
+++ src/main/java/thread/FunDemo05.java
@@ -28,7 +28,12 @@ public class FunDemo05 {
 //            t1.join(); // 线程的合并，和主线程合并  相当于我们直接调用了run方法
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
-//        }
+//
+        //111
+
+
+
+
         System.out.println("main end ...");
     }
 }
---
+++ src/main/java/thread/GitTset.java
@@ -0,0 +1,9 @@
+package thread;
+
+/**
+ * @author 江朋俊
+ * @create 2023-04-16 21:57
+ */
+public class GitTset {
+    String name;
+}
